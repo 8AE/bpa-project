03df33928b9396ffcd061ae0907863c3f56e52d9
noah-curran <swimnoah113@gmail.com>
2017-01-23 13:11:43 -0500

    Update Weapon.java

--- bpa-project/java-rpg-master/Weapon.java
+++ bpa-project/java-rpg-master/Weapon.java
@@ -7,13 +7,20 @@
 public abstract class Weapon implements Common {
     protected int tileRange;
     protected int damage;
-      protected int id;
-      protected String name;
+    protected int id;
+    protected String name;
      
-      public Weapon(String name, int damage, int id, int tileRange){
-          this.name = name;
+    public Weapon(String name, int damage, int id, int tileRange){
+        this.name = name;
      
-      }
+    }
      
+    public int getDamage() {
+        return this.damage;
+    }
+    
+    public int getRange() {
+        return this.tileRange;
+    }
       
 }
